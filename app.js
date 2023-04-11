77aa63fadf034dd565c2ab5bb122b820b2d5cde7
nalinikantheti <nalinikantheti@gmail.com>
2023-04-11 08:26:33 -0400

    fix

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
+import tuits from "./controllers/tuits/tuits.js";
+
 const app = express()
 app.use(cors())
 app.use(express.json());
 TuitsController(app);
 HelloController(app)
 UserController(app)
-app.listen(process.env.PORT || 4000)
 
 const DB_CONNECION_STRING = "mongodb+srv://nalinikantheti:NaliniV1RHO2J8@cluster0.d6scmxa.mongodb.net/?retryWrites=true&w=majority";
 const CONNECTION_STRING = process.env.DB_CONNECION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+
+console.log(CONNECTION_STRING)
+
 mongoose.connect(CONNECTION_STRING);
+
+app.listen(process.env.PORT || 4000)
